cd5cf7ca14517c09b7cff6d5a15aa3eac8ce1702
Ricardo Borges <ricardoborges@gmail.com>
2026-05-23 15:44:47 -0300

    cors

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -10,10 +10,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
